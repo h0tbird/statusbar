5b930aa8c64383954596e21d965ace438d9f1e10
Marc Villacorta <marc.villacorta@gmail.com>
2017-12-28 18:36:28 +0100

    Get battery percentage

--- battery.go
+++ battery.go
@@ -4,15 +4,49 @@ package main
 // Imports:
 //-----------------------------------------------------------------------------
 
-import "time"
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"time"
+)
 
 //-----------------------------------------------------------------------------
 // battery:
 //-----------------------------------------------------------------------------
 
 func battery(i *item) {
+
+	var eNow, eFull, perc int
+	const path = "/sys/class/power_supply/BAT0/"
+
 	for {
-		i.data = softPurple + iconBattery + "100%"
+
+		// Energy now:
+		energyNow, err := ioutil.ReadFile(path + "energy_now")
+		if err != nil {
+			eNow = -1
+		} else if _, err := fmt.Sscanf(string(energyNow), "%d", &eNow); err != nil {
+			eNow = -1
+		}
+
+		// Energy full:
+		energyFull, err := ioutil.ReadFile(path + "energy_full")
+		if err != nil {
+			eFull = -1
+		} else if _, err := fmt.Sscanf(string(energyFull), "%d", &eFull); err != nil {
+			eFull = -1
+		}
+
+		// Percentage:
+		if (eNow == -1) || (eFull == -1) {
+			perc = -1
+		} else {
+			perc = eNow * 100 / eFull
+		}
+
+		// Set and sleep:
+		i.data = softPurple + iconBattery + strconv.Itoa(perc) + "%"
 		time.Sleep(time.Minute)
 	}
 }
